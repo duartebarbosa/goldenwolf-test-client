207ae5d60178a676c39ce67e7905e182192ab972
Oliver Tupman <oliver.tupman@centralway.com>
2013-07-28 22:24:25 +0200

    api setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var exec = require('child_process').exec
+  , fs = require('fs');
+
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -11,8 +14,20 @@ module.exports = function(grunt) {
           "password": ""
         }
       }
-    },
-    zip: {
+    }
+    , exec: {
+      clone_api: {
+        cmd: "git clone https://github.com/otupman/goldenwolf-test-api api/"
+      }
+      , start_api: {
+        cmd: "rails s -d"
+        , cwd: "api"
+      }
+      , stop_api: {
+        cmd: "kill `cat tmp/pids/server.pid`"
+      }
+    }
+    , zip: {
       app: {
         src: ["index.html", "Config.xml", "components/**/*.*", "partials/*.html", "app.js", "controller.js"],
         dest: "app.zip"
@@ -23,8 +38,13 @@ module.exports = function(grunt) {
   // Load local tasks.
   grunt.loadNpmTasks('grunt-zipstream');
   grunt.loadNpmTasks('grunt-phonegap-build');
-
+  grunt.loadNpmTasks('grunt-exec');
+  
   // Default task.
   grunt.registerTask('default', ['zip', 'phonegap-build']);
+  
+  grunt.registerTask('setup-api', 'Sets up the API', [
+    "exec:clone_api"  
+  ]);
 };
 
